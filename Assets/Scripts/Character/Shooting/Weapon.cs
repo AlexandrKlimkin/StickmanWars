31f04c2a7ada6927e6cffaa4dd67c6a734720ee5
KlimkinAlexandr <aexklim@gmail.com>
2020-01-19 18:27:20 +0300

    wepon classes templates

---
+++ Assets/Scripts/Character/Shooting/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWeapon
+{
+    WeaponConfig Stats { get; }
+    void Fire();
+}
+
+public abstract class Weapon : MonoBehaviour, IWeapon
+{
+    public WeaponConfig Stats => _Stats;
+    public abstract void Fire();
+
+    [SerializeField]
+    private WeaponConfig _Stats;
+}
