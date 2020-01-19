c697a8fd6314c39ba971e6f457a70a4ab1afe17a
KlimkinAlexandr <aexklim@gmail.com>
2020-01-19 14:02:54 +0300

    test aiming

---
+++ Assets/Scripts/Character/Shooting/WeaponController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public Transform NearArmTransform;
+
+    private Camera _Camera;
+
+    private void Start()
+    {
+        _Camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        NearArmTransform.position = _Camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
