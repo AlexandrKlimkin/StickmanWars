12e09a784efe9b35f67312b681e6dde61e4c05e3
KliM <AexKlim@gmail.com>
2020-05-09 02:58:49 +0300

    yuri, weapons

---
+++ Assets/Scripts/Core/Services/CharacterSelection/CharacterSelectionService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Services.Game;
+using KlimLib.SignalBus;
+using Tools.Services;
+using UnityDI;
+
+namespace Core.Services.MapSelection {
+    public class CharacterSelectionService : ILoadableService, IUnloadableService {
+
+        [Dependency]
+        private readonly SignalBus _SignalBus;
+        [Dependency]
+        private readonly CharacterCreationService _CharacterCreationService;
+
+        public void Load() { }
+        public void Unload() { }
+
+        public void SelectCharacter() {
+
+        }
+    }
+}
