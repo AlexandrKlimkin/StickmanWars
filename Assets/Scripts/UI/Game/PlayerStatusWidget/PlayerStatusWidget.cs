802c3047b9f6933c19bab29a405945c39d7449b5
KlimkinAlexandr <aexklim@gmail.com>
2020-06-21 10:38:59 +0300

    player widget templates, weapon

---
+++ Assets/Scripts/UI/Game/PlayerStatusWidget/PlayerStatusWidget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Game {
+    public class PlayerStatusWidget : MonoBehaviour {
+        public Text KillsCountText;
+        public Transform LifesContainer;
+        public Image Avatar;
+        public Image HealthImage;
+
+
+    }
+}
