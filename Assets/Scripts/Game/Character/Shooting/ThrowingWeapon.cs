88de087cf822b6811c6320aaaf27837c0629a626
Danil <mrgolemms@gmail.com>
2020-08-02 21:32:22 +0900

    Добавил бросок гранаты

--- Assets/Scripts/Game/Character/Shooting/ThrowingWeapon.cs
+++ Assets/Scripts/Game/Character/Shooting/ThrowingWeapon.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.Scripts.Tools;
 using Character.Shooting.Character.Shooting;
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 namespace Character.Shooting {
@@ -7,23 +9,21 @@ namespace Character.Shooting {
         public override WeaponInputProcessor InputProcessor => _FireForceProcessor ?? (_FireForceProcessor = new FireForceProcessor(this));
         private FireForceProcessor _FireForceProcessor;
 
+        private float _startTime;
+        private bool _throw = false;
+        private Vector2 _dir;
+        [SerializeField] private GrenadeProjectile _grenadeScript;
+        [SerializeField] private Rigidbody2D _rb2d;
         public bool CanBePicked { get; private set; }
 
         protected override bool UseThrowForce => false;
+
 
         public override ThrowingProjectileData GetProjectileData() {
             var data = base.GetProjectileData();
             data.BirthTime = Time.time;
             data.LifeTime = Stats.Range / Stats.ProjectileSpeed;
             data.Position = transform.position;
-
-            Vector3 shootRotEuler;
-            if (PickableItem.Owner != null) {
-                var directionVector = PickableItem.Owner.WeaponController.AimPosition - WeaponView.ShootTransform.position.ToVector2().normalized;
-                data.StartDirection = directionVector;
-
-                data.StartVelocity = Mathf.Lerp(_Stats.MinThrowStartSpeed, _Stats.MaxThrowStartSpeed, _FireForceProcessor.NormilizedForce);
-            }
             data.Rotation = transform.rotation;
             return data;
         }
@@ -36,9 +36,34 @@ namespace Character.Shooting {
             base.PerformShot();
             var projectile = GetProjectile();
             var data = GetProjectileData();
-            PickableItem.Owner.WeaponController.ThrowOutMainWeapon(data.StartVelocity, -720f);
             projectile.Setup(data);
+            _dir = PickableItem.Owner.WeaponController.AimPosition;
+            PickableItem.Owner.WeaponController.ThrowOutMainWeapon(data.StartVelocity, -720f);
+            _throw = true;
             projectile.Play();
+        }
+
+        public void FixedUpdate()
+        { 
+            if (_throw == true)
+            {
+             _rb2d.AddForce(_dir.normalized * (_startTime * 500f), ForceMode2D.Impulse);
+                _throw = false;
+                _startTime = 0;
+            }
+            
+        }
+        public void Update()
+        {
+            if (PickableItem.Owner != null && Input.GetMouseButton(0))
+            {              
+                _startTime += Time.deltaTime; 
+                if (_startTime >= _grenadeScript.Timer)
+                {
+                    Debug.Log(_startTime);
+                    PerformShot();
+                }
+            }
         }
     }
 }
