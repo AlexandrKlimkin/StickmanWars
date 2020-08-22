e16bcb8d3e33f4f1ae93d4202cd933ecadcfe429
Danil <mrgolemms@gmail.com>
2020-08-22 16:51:00 +0900

    New map SewerBranch

---
+++ Assets/Scripts/SewerBranch/GateController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    [SerializeField] private GameObject _gate;
+    [SerializeField] private GameObject _flootButtun;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _gate.SetActive(false);
+            _flootButtun.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("floor buttun make sound! Click!");
+    }
+
+}
+
---
+++ Assets/Scripts/SewerBranch/WaterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject _water;
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector2 _endPosition;
+
+    private Vector2 _startPosition;
+    private float _progress;
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector2.Lerp(_startPosition, _endPosition, _progress);
+        _progress += _speed;
+    }
+
+}
