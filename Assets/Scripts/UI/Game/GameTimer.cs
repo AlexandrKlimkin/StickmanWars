1eec98524ca2e18e956054aea59983412d5bf9c7
Danil <mrgolemms@gmail.com>
2020-10-10 12:11:55 +0900

    GameTimerController

--- Assets/Scripts/UI/Game/GameTimer.cs
+++ Assets/Scripts/UI/Game/GameTimer.cs
@@ -1,9 +1,7 @@
-﻿using Game.Match;
-using KlimLib.SignalBus;
+﻿
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityDI;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,43 +10,7 @@ public class GameTimer : MonoBehaviour
     [SerializeField] private float seconds, minutes, hours;
     [SerializeField] private Text timerDisplay;
 
-    [Dependency]
-    private readonly SignalBus _SignalBus;
-
-    private void Awake()
-    {
-        ContainerHolder.Container.BuildUp(this);
-        _SignalBus.Subscribe<MatchEndSignal>(OnMatchEnd, this);
-    }
-
-
-    private void Start()
-    {
-        StartTimer();
-    }
-
-    private void OnMatchEnd (MatchEndSignal signal)
-    {
-        StopTimer();
-    }
-    public void StartTimer ()
-    {
-        StopAllCoroutines();
-        StartCoroutine(Timer());
-    }
-
-    public void StopTimer()
-    {
-        StopAllCoroutines();
-    }
-
-    private void OnDestroy()
-    {
-        _SignalBus.UnSubscribe<MatchEndSignal>(this);
-    }
-
-
-    IEnumerator Timer()
+    public IEnumerator Timer()
     {
         while (true)
         {
---
+++ Assets/Scripts/UI/Game/GameTimerController.cs
@@ -0,0 +1,47 @@
+﻿using Game.Match;
+using KlimLib.SignalBus;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityDI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimerController : MonoBehaviour
+{
+    [SerializeField] private GameTimer _timer;
+
+    [Dependency]
+    private readonly SignalBus _SignalBus;
+
+    private void Awake()
+    {
+        ContainerHolder.Container.BuildUp(this);
+        _SignalBus.Subscribe<MatchEndSignal>(OnMatchEnd, this);
+    }
+
+    private void Start()
+    {
+        StartTimer();
+    }
+
+    private void OnMatchEnd(MatchEndSignal signal)
+    {
+        StopTimer();
+    }
+    public void StartTimer()
+    {
+        StopAllCoroutines();
+        StartCoroutine(_timer.Timer());
+    }
+
+    public void StopTimer()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnDestroy()
+    {
+        _SignalBus.UnSubscribe<MatchEndSignal>(this);
+    }
+}
